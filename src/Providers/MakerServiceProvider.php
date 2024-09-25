deb146ae6d4708a28f0bf4529eb5ae064732e9f4
Joynal Abedin <joynal.a@razinsoft.com>
2024-09-25 11:53:23 +0600

    maker package init

--- src/Providers/MakerServiceProvider.php
+++ src/Providers/MakerServiceProvider.php
@@ -2,11 +2,10 @@
 
 namespace Abedin\Boiler\Providers;
 
-use Abedin\Boiler\Commands\MakeAuth;
 use Abedin\Boiler\Commands\MakeModel;
 use Illuminate\Support\ServiceProvider;
 
-class BoilerServiceProvider extends ServiceProvider
+class MakerServiceProvider extends ServiceProvider
 {
 
     /**
@@ -15,18 +14,11 @@ class BoilerServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->registerNewModelCommand();
-        $this->registerMakeAuthCommand();
     }
 
     protected function registerNewModelCommand(): void
     {
         $this->app->bind('command.make:model', MakeModel::class);
         $this->commands(['command.make:model']);
-    }
-
-    protected function registerMakeAuthCommand()
-    {
-        $this->app->bind('command.auth:generate', MakeAuth::class);
-        $this->commands(['command.auth:generate']);
     }
 }
