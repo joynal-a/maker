29951cef1fff6afd797a7e1cf0127745e9a891b2
Joynal Abedin <abedin.dev@gmail.com>
2025-10-26 13:39:29 +0600

    add markate place in this package

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Abedin\Maker\Controllers\MarketController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('warning', function () {
     return view('joynala.maker::warning');
 })->name('warning');
+
+Route::controller(MarketController::class)->middleware('auth', config('installer.admin_role'))->group(function () {
+    Route::get('/admin/upgrade-notice', 'upgrade')->name('marketplace.upgrade');
+    Route::get('/admin/marketplace', 'index')->name('marketplace.index');
+});
